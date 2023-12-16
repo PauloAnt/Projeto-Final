54bee6243377a07dccfa3b091c7d44ed09925d92
Rodrigo Padilha <151454973+rodrigoPadi@users.noreply.github.com>
2023-12-16 19:14:32 -0300

    Update HistoricoSearch.py

--- HistoricoSearch.py
+++ HistoricoSearch.py
@@ -5,18 +5,31 @@ class HistorySearchException(Exception):
         super().__init__(msg)
 
 class HistorySearch:
-    def __init__(self):
-        self.__pilha = Pilha(5)
-    
+    def __init__(self, limit=5):
+        self.__pilha = Pilha()
+        self.__limit = limit
+
     def __str__(self):
         return f'{", ".join(map(str, self.get_history()))}'
     
     def add_search(self, search):
-        self.__pilha.empilha(search)
-
+        if len(self.__pilha) == self.__limit:
+            self.__pilha.desempilha()
+        self.__pilha = self.__rearrange_history(search)
+        
     def remove_search(self):
-        if self.__pilha.estaVazia():
+        if not self.__pilha.estaVazia():
             self.__pilha.desempilha()
 
     def get_history(self):
         return list(self.__pilha)
+
+    def __rearrange_history(self, new_search):
+        support_pilha = Pilha()
+        while not self.__pilha.estaVazia():
+            support_pilha.empilha(self.__pilha.desempilha())
+
+        support_pilha.empilha(new_search)
+        while not support_pilha.estaVazia():
+            self.__pilha.empilha(support_pilha.desempilha())
+        return self.__pilha
